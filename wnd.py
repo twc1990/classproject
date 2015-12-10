31c49bfbb39006e88e0a07d558dc3b75654180ff
Alva Connor Waters <connor_waters@hotmail.com>
2015-12-09 22:24:01 -0500

    Getting things working in the most rudimentary manner

--- wnd.py
+++ wnd.py
@@ -26,5 +26,61 @@ def initial_password(submit):
     wnd.bind("<Return>", lambda e, b=btn: b.invoke())
     wnd.mainloop()
 
-def main_view():
-    pass
+def view_pw(pwmap, key):
+    wnd = Tk()
+    wnd.title("Password")
+
+    frame = Frame(wnd)
+
+    pw = StringVar(value="123456")
+
+    label = Label(frame, text=key + ": ")
+    box = Label(frame, text=pwmap[key])
+    btn = Button(frame, text="Close", command=lambda w=wnd: w.destroy())
+
+    frame.grid(column=0, row=0)
+    label.grid(column=0, row=0, columnspan=1)
+    box.grid(column=1, row=0, columnspan=1)
+    btn.grid(column=0, row=1, columnspan=2)
+
+    wnd.mainloop()
+
+def edit_pw(pwmap, modify, key):
+    modify(pwmap, key)
+
+def add_pw(pwmap, modify, key):
+    modify(pwmap, key)
+
+def main_view(pwmap, pwmodify, options):
+    wnd = Tk()
+    wnd.title("Password Manager")
+
+    frame = Frame(wnd)
+
+    tbl = Listbox(frame)
+
+    for key in pwmap.keys():
+        tbl.insert(END, key)
+
+    scl = Scrollbar(frame, orient=VERTICAL, command=tbl.yview)
+    tbl.configure(yscrollcommand=scl.set)
+
+    view = Button(frame, text="View", default="active", command=lambda: view_pw(pwmap, tbl.get(tbl.curselection())))
+    edit = Button(frame, text="Edit", command=lambda: edit_pw(pwmap, pwmodify, tbl.get(tbl.curselection())))
+    add  = Button(frame, text="Add", command=lambda: add_pw(pwmap, pwmodify, tbl.get(tbl.curselection())))
+    options = Button(frame, text="Options", command=lambda: options_menu(options))
+
+    frame.grid(column=0, row=0)
+    
+    tbl.grid(column=1, row=1, columnspan=3, rowspan=4)
+    scl.grid(column=4, row=1, columnspan=1, rowspan=4, sticky=(N,S))
+
+    view.grid(column=5, row=1, columnspan=1, rowspan=1)
+    edit.grid(column=5, row=2, columnspan=1, rowspan=1)
+    add.grid(column=5, row=3, columnspan=1, rowspan=1)
+    options.grid(column=5, row=4, columnspan=1, rowspan=1)
+
+    wnd.mainloop()
+
+#initial_password(lambda x: print(str(x)))
+main_view({"test":"test", "asdf":"jkl;", "qwerty":"uiop"}, lambda x,y: print(y + ": " + x[y]), lambda x: print("Options!"))
