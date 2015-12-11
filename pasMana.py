b5174a4e2e64fda74f36b7a2b9f3c5516395f304
twc1990 <twcontes@gmail.com>
2015-12-11 15:53:14 -0500

    stuff

--- pasMana.py
+++ pasMana.py
@@ -3,5 +3,7 @@ from gen import *
 
 class pasMana():
     #initial_password(lambda x: print(str(x)))
-    #cryptTest("password")
-    #decrTest("password")
+    setPass("password")
+    crypt("password")
+    DIC=decrTest()
+    cryptTest(DIC)
