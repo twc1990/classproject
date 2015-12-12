2b1a3a9cf61a64bb5bc3c7dfd7c194859d68d91e
Alva Connor Waters <connor_waters@hotmail.com>
2015-12-11 21:04:53 -0500

    Cleanup

--- pasMana.py
+++
@@ -1,9 +0,0 @@
-from wnd import *
-from gen import *
-
-class pasMana():
-    #initial_password(lambda x: print(str(x)))
-    setPass("password")
-    crypt()
-    DIC=decrTest()
-    cryptTest(DIC)
